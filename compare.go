ff2010cb9a924fdb677e94d54c3c4af4ac2de99c
Andy Kreling <krelinga@gmail.com>
2026-03-29 03:08:42 +0000

    comparison operations.

---
+++ compare.go
@@ -0,0 +1,96 @@
+package lego
+
+import (
+	"cmp"
+	"slices"
+)
+
+type Comparer[T any] interface {
+	Compare(T) int
+}
+
+// A CmpFunc is a function that compares two values of the same type and returns an integer indicating their relative order.
+// The function should return a negative integer if the first value is less than the second value, a positive integer if the first value is greater than the second value, and zero if the two values are equal.
+type CmpFunc[T any] func(T, T) int
+
+// CompareUsing compares two values using the given comparison functions.
+// The comparison functions are tried in order until one of them returns a non-zero value, which is returned by this function.
+// If all comparison functions return zero, then this function returns zero.
+//
+// This function is useful for implementing the [Comparer] interface on a type that has multiple fields that need to be compared, since it allows you to write the comparison logic for each field separately and then combine them easily.
+func CompareUsing[T any](a, b T, funcs ...CmpFunc[T]) int {
+	for _, f := range funcs {
+		if x := f(a, b); x != 0 {
+			return x
+		}
+	}
+	return 0
+}
+
+// NewCmpFunc creates a CmpFunc that compares two values by applying the given function to each value and then comparing the results using the Compare method of the results.
+func NewCmpFunc[T any, F Comparer[F]](g func(T) F) CmpFunc[T] {
+	return func(a, b T) int {
+		return g(a).Compare(g(b))
+	}
+}
+
+// NewCmpFuncGo creates a CmpFunc that compares two values by applying the given function to each value and then comparing the results using the cmp.Compare function from the cmp package.
+func NewCmpFuncGo[T any, F cmp.Ordered](g func(T) F) CmpFunc[T] {
+	return func(a, b T) int {
+		return cmp.Compare(g(a), g(b))
+	}
+}
+
+// NewCmpFuncUsing creates a CmpFunc that compares two values using the given comparison functions, which are tried in order until one of them returns a non-zero value, which is returned by the resulting CmpFunc. If all comparison functions return zero, then the resulting CmpFunc returns zero.
+func NewCmpFuncUsing[T any](funcs ...CmpFunc[T]) CmpFunc[T] {
+	return func(a, b T) int {
+		return CompareUsing(a, b, funcs...)
+	}
+}
+
+// Equal returns true if a and b are equal according to the Compare method of the [Comparer] interface, and false otherwise.
+func Equal[T Comparer[T]](a, b T) bool {
+	return a.Compare(b) == 0
+}
+
+// Less returns true if a is less than b according to the Compare method of the [Comparer] interface, and false otherwise.
+func Less[T Comparer[T]](a, b T) bool {
+	return a.Compare(b) < 0
+}
+
+// LessEqual returns true if a is less than or equal to b according to the Compare method of the [Comparer] interface, and false otherwise.
+func LessEqual[T Comparer[T]](a, b T) bool {
+	return a.Compare(b) <= 0
+}
+
+// Greater returns true if a is greater than b according to the Compare method of the [Comparer] interface, and false otherwise.
+func Greater[T Comparer[T]](a, b T) bool {
+	return a.Compare(b) > 0
+}
+
+// GreaterEqual returns true if a is greater than or equal to b according to the Compare method of the [Comparer] interface, and false otherwise.
+func GreaterEqual[T Comparer[T]](a, b T) bool {
+	return a.Compare(b) >= 0
+}
+
+// Sort sorts the elements of the given [Slice] in place using the Compare method of the [Comparer] interface to determine the order of the elements.
+func Sort[T Comparer[T]](s Slice[T]) {
+	slices.SortFunc(s.s, func(a, b T) int {
+		return a.Compare(b)
+	})
+}
+
+// SortFunc sorts the elements of the given [Slice] in place using the given CmpFunc to determine the order of the elements.
+func SortFunc[T any](s Slice[T], compare CmpFunc[T]) {
+	slices.SortFunc(s.s, compare)
+}
+
+// SortGo sorts the elements of the given [Slice] in place using the natural order of the elements, which must implement the [cmp.Ordered] interface.
+func SortGo[T cmp.Ordered](s Slice[T]) {
+	slices.Sort(s.s)
+}
+
+// Reverse reverses the elements of the given [Slice] in place.
+func Reverse[T any](s Slice[T]) {
+	slices.Reverse(s.s)
+}
