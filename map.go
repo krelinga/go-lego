222bbc17bc48b8601e9cd7eb69ee0448da8bca35
Andy Kreling <krelinga@gmail.com>
2026-03-30 18:41:03 +0000

    remove return value from Map.Set().
    callers can already use lego.Has() to perform a similar opreation.

--- map.go
+++ map.go
@@ -50,14 +50,12 @@ func (m *Map[K, V]) Add(pair Pair[K, V]) {
 	m.GoMap[pair.GetKey()] = pair.GetValue()
 }
 
-// Set adds the key and value to the map, and returns true if the key already existed in the map, and false otherwise.
-func (m *Map[K, V]) Set(key K, value V) bool {
+// Set stores the given value in the map under the given key, replacing any existing value for that key.
+func (m *Map[K, V]) Set(key K, value V) {
 	if m.GoMap == nil {
 		m.GoMap = GoMap[K, V]{}
 	}
-	_, exists := m.GoMap[key]
 	m.GoMap[key] = value
-	return exists
 }
 
 // Reserve reserves space for n elements in the map. This is a best-effort operation and will do nothing if the map already contains some values, since Go's built-in maps do not support reserving space after initialization.
