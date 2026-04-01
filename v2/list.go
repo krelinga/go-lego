3e4109962f4b65d5637e0ce1217d9a176a7b9a98
Andy Kreling <krelinga@gmail.com>
2026-04-01 15:27:21 +0000

    remove the concept of Readonly from List.

--- v2/list.go
+++ v2/list.go
@@ -2,18 +2,17 @@ package v2
 
 import "iter"
 
-type ListView[K1, V1, K2, V2 any] interface {
+type ListView[K, V any] interface {
 	Length() int
-	Get(K1) (V1, bool)
-	All() iter.Seq2[K1, V1]
-	First() (K1, bool)
-	Last() (K1, bool)
-	Readonly() ListView[K2, V2, K2, V2]
+	Get(K) (V, bool)
+	All() iter.Seq2[K, V]
+	First() (K, bool)
+	Last() (K, bool)
 }
 
-type List[K1, V1, K2, V2 any] interface {
-	ListView[K1, V1, K2, V2]
-	InsertBefore(K1, V1)
-	InsertAfter(K1, V1)
-	Remove(K1)
+type List[K, V any] interface {
+	ListView[K, V]
+	InsertBefore(K, V)
+	InsertAfter(K, V)
+	Remove(K)
 }
--- v2/slicelist.go
+++ v2/slicelist.go
@@ -5,103 +5,60 @@ import (
 	"slices"
 )
 
-type sliceList[V1, V2 any] struct {
-	slice []V1
-	valueReadonly ReadonlyFunc[V1, V2]
+type sliceList[V any] struct {
+	slice []V
 }
 
-func (l *sliceList[V1, V2]) Length() int {
+func (l *sliceList[V]) Length() int {
 	return len(l.slice)
 }
 
-func (l *sliceList[V1, V2]) Get(k int) (V1, bool) {
+func (l *sliceList[V]) Get(k int) (V, bool) {
 	if k < 0 || k >= len(l.slice) {
-		var zero V1
+		var zero V
 		return zero, false
 	}
 	return l.slice[k], true
 }
 
-func (l *sliceList[V1, V2]) All() iter.Seq2[int, V1] {
+func (l *sliceList[V]) All() iter.Seq2[int, V] {
 	return slices.All(l.slice)
 }
 
-func (l *sliceList[V1, V2]) First() (int, bool) {
+func (l *sliceList[V]) First() (int, bool) {
 	return 0, len(l.slice) > 0
 }
 
-func (l *sliceList[V1, V2]) Last() (int, bool) {
+func (l *sliceList[V]) Last() (int, bool) {
 	if len(l.slice) == 0 {
 		return 0, false
 	}
 	return len(l.slice) - 1, true
 }
 
-func (l *sliceList[V1, V2]) Readonly() ListView[int, V2, int, V2] {
-	return sliceListReadonly[V1, V2]{
-		sliceList: l,
-	}
-}
-
-func (l *sliceList[V1, V2]) InsertBefore(k int, v V1) {
+func (l *sliceList[V]) InsertBefore(k int, v V) {
 	if k < 0 || k > len(l.slice) {
 		panic("index out of bounds")
 	}
-	l.slice = append(l.slice[:k], append([]V1{v}, l.slice[k:]...)...)
+	l.slice = append(l.slice[:k], append([]V{v}, l.slice[k:]...)...)
 }
 
-func (l *sliceList[V1, V2]) InsertAfter(k int, v V1) {
+func (l *sliceList[V]) InsertAfter(k int, v V) {
 	if k < 0 || k >= len(l.slice) {
 		panic("index out of bounds")
 	}
-	l.slice = append(l.slice[:k+1], append([]V1{v}, l.slice[k+1:]...)...)
+	l.slice = append(l.slice[:k+1], append([]V{v}, l.slice[k+1:]...)...)
 }
 
-func (l *sliceList[V1, V2]) Remove(k int) {
+func (l *sliceList[V]) Remove(k int) {
 	if k < 0 || k >= len(l.slice) {
 		panic("index out of bounds")
 	}
 	l.slice = append(l.slice[:k], l.slice[k+1:]...)
 }
 
-func NewSliceList[V1 any](entries ...V1) List[int, V1, int, V1] {
-	return &sliceList[V1, V1]{
+func NewSliceList[V any](entries ...V) List[int, V] {
+	return &sliceList[V]{
 		slice: entries,
-		valueReadonly: Identity[V1],
 	}
-}
-
-func NewSliceListReadonly[V1, V2 any](valueReadonly ReadonlyFunc[V1, V2], entries ...V1) List[int, V1, int, V2] {
-	return &sliceList[V1, V2]{
-		slice: entries,
-		valueReadonly: valueReadonly,
-	}
-}
-
-type sliceListReadonly[V1, V2 any] struct {
-	*sliceList[V1, V2]
-}
-
-func (l sliceListReadonly[V1, V2]) Get(k int) (V2, bool) {
-	v1, ok := l.sliceList.Get(k)
-	if !ok {
-		var zero V2
-		return zero, false
-	}
-	return l.valueReadonly(v1), true
-}
-
-func (l sliceListReadonly[V1, V2]) All() iter.Seq2[int, V2] {
-	return func(yield func(int, V2) bool) {
-		for i, v1 := range l.sliceList.slice {
-			v2 := l.valueReadonly(v1)
-			if !yield(i, v2) {
-				return
-			}
-		}
-	}
-}
-
-func (l sliceListReadonly[V1, V2]) Readonly() ListView[int, V2, int, V2] {
-	return l
 }
