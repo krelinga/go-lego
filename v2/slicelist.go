bf3c8414aad4adee4c7326935b7f76cee235cc2f
Andy Kreling <krelinga@gmail.com>
2026-04-01 15:30:02 +0000

    add SliceList as an exposed type.

--- v2/slicelist.go
+++ v2/slicelist.go
@@ -5,15 +5,15 @@ import (
 	"slices"
 )
 
-type sliceList[V any] struct {
+type SliceList[V any] struct {
 	slice []V
 }
 
-func (l *sliceList[V]) Length() int {
+func (l *SliceList[V]) Length() int {
 	return len(l.slice)
 }
 
-func (l *sliceList[V]) Get(k int) (V, bool) {
+func (l *SliceList[V]) Get(k int) (V, bool) {
 	if k < 0 || k >= len(l.slice) {
 		var zero V
 		return zero, false
@@ -21,48 +21,48 @@ func (l *sliceList[V]) Get(k int) (V, bool) {
 	return l.slice[k], true
 }
 
-func (l *sliceList[V]) All() iter.Seq2[int, V] {
+func (l *SliceList[V]) All() iter.Seq2[int, V] {
 	return slices.All(l.slice)
 }
 
-func (l *sliceList[V]) First() (int, bool) {
+func (l *SliceList[V]) First() (int, bool) {
 	return 0, len(l.slice) > 0
 }
 
-func (l *sliceList[V]) Last() (int, bool) {
+func (l *SliceList[V]) Last() (int, bool) {
 	if len(l.slice) == 0 {
 		return 0, false
 	}
 	return len(l.slice) - 1, true
 }
 
-func (l *sliceList[V]) InsertBefore(k int, v V) {
+func (l *SliceList[V]) InsertBefore(k int, v V) {
 	if k < 0 || k > len(l.slice) {
 		panic("index out of bounds")
 	}
 	l.slice = append(l.slice[:k], append([]V{v}, l.slice[k:]...)...)
 }
 
-func (l *sliceList[V]) InsertAfter(k int, v V) {
+func (l *SliceList[V]) InsertAfter(k int, v V) {
 	if k < 0 || k >= len(l.slice) {
 		panic("index out of bounds")
 	}
 	l.slice = append(l.slice[:k+1], append([]V{v}, l.slice[k+1:]...)...)
 }
 
-func (l *sliceList[V]) Append(v V) {
+func (l *SliceList[V]) Append(v V) {
 	l.slice = append(l.slice, v)
 }
 
-func (l *sliceList[V]) Remove(k int) {
+func (l *SliceList[V]) Remove(k int) {
 	if k < 0 || k >= len(l.slice) {
 		panic("index out of bounds")
 	}
 	l.slice = append(l.slice[:k], l.slice[k+1:]...)
 }
 
-func NewSliceList[V any](entries ...V) List[int, V] {
-	return &sliceList[V]{
+func NewSliceList[V any](entries ...V) *SliceList[V] {
+	return &SliceList[V]{
 		slice: entries,
 	}
 }
