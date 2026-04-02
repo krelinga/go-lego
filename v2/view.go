473355fbb094a4e1379eadee55195ffe8aa238f9
Andy Kreling <krelinga@gmail.com>
2026-04-02 15:45:51 +0000

    first try at a ViewHelperListOfViewers().

--- v2/view.go
+++ v2/view.go
@@ -4,16 +4,49 @@ type Viewer[V any] interface {
 	View() V
 }
 
-type ViewMixinTrivial[P, V any] struct {
-	fixedList FixedList[P, V]
+func ViewHelperListOfViewers[P any, T Viewer[V], V any](in FixedList[P, T]) FixedList[P, V] {
+	return viewHelperListOfViewers[P, T, V]{
+		FixedList: in,
+	}
 }
 
-func (m ViewMixinTrivial[P, V]) View() FixedList[P, V] {
-	return m.fixedList
+type viewHelperListOfViewers[P any, T Viewer[V], V any] struct {
+	FixedList[P, T]
 }
 
-type ViewMixinListOfDirectValues[P, V any, FL FixedList[P, V]] func() FL
+func (h viewHelperListOfViewers[P, T, V]) Get(p P) (V, bool) {
+	viewer, ok := h.FixedList.Get(p)
+	if !ok {
+		var zero V
+		return zero, false
+	}
+	return viewer.View(), true
+}
 
-func (m ViewMixinListOfDirectValues[P, V, FL]) View() FL {
-	return m()
+func (h viewHelperListOfViewers[P, T, V]) List() ListSeq[P, V] {
+	return func(yield func(P, V) bool) {
+		for p, viewer := range h.FixedList.List() {
+			if !yield(p, viewer.View()) {
+				return
+			}
+		}
+	}
+}
+
+func (h viewHelperListOfViewers[P, T, V]) First() (P, V, bool) {
+	p, viewer, ok := h.FixedList.First()
+	if !ok {
+		var zero V
+		return p, zero, false
+	}
+	return p, viewer.View(), true
+}
+
+func (h viewHelperListOfViewers[P, T, V]) Last() (P, V, bool) {
+	p, viewer, ok := h.FixedList.Last()
+	if !ok {
+		var zero V
+		return p, zero, false
+	}
+	return p, viewer.View(), true
 }
