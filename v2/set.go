8ed2c10b423128e05dc67feb9cb3769a6a17eb90
Andy Kreling <krelinga@gmail.com>
2026-04-03 21:54:37 +0000

    set interface.

---
+++ v2/set.go
@@ -0,0 +1,16 @@
+package v2
+
+import "iter"
+
+type FixedSet[V any] interface {
+	Length() int
+	Has(V) bool
+	All() iter.Seq[V]
+	String() string
+}
+
+type Set[V any] interface {
+	FixedSet[V]
+	Add(V)
+	Remove(V)
+}
