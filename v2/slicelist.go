6f72eed59e466d4b9a33fcd365e3f9fdedbd770e
Andy Kreling <krelinga@gmail.com>
2026-04-03 02:19:48 +0000

    simplify SliceList to just be a slice.

--- v2/slicelist.go
+++ v2/slicelist.go
@@ -5,29 +5,27 @@ import (
 	"slices"
 )
 
-type SliceList[V any] struct {
-	slice []V
-}
+type SliceList[V any] []V
 
 func (l *SliceList[V]) Length() int {
-	return len(l.slice)
+	return len(*l)
 }
 
 func (l *SliceList[V]) Get(p int) (V, bool) {
-	if p < 0 || p >= len(l.slice) {
+	if p < 0 || p >= len(*l) {
 		var zero V
 		return zero, false
 	}
-	return l.slice[p], true
+	return (*l)[p], true
 }
 
 func (l *SliceList[V]) All() iter.Seq2[int, V] {
-	return slices.All(l.slice)
+	return slices.All(*l)
 }
 
 func (l *SliceList[V]) Positions() iter.Seq[int] {
 	return func(yield func(int) bool) {
-		for i := range l.slice {
+		for i := range *l {
 			if !yield(i) {
 				return
 			}
@@ -36,13 +34,13 @@ func (l *SliceList[V]) Positions() iter.Seq[int] {
 }
 
 func (l *SliceList[V]) Values() iter.Seq[V] {
-	return slices.Values(l.slice)
+	return slices.Values(*l)
 }
 
 func (l *SliceList[V]) ReverseAll() iter.Seq2[int, V] {
 	return func(yield func(int, V) bool) {
-		for i := len(l.slice) - 1; i >= 0; i-- {
-			if !yield(i, l.slice[i]) {
+		for i := len(*l) - 1; i >= 0; i-- {
+			if !yield(i, (*l)[i]) {
 				return
 			}
 		}
@@ -51,7 +49,7 @@ func (l *SliceList[V]) ReverseAll() iter.Seq2[int, V] {
 
 func (l *SliceList[V]) ReversePositions() iter.Seq[int] {
 	return func(yield func(int) bool) {
-		for i := len(l.slice) - 1; i >= 0; i-- {
+		for i := len(*l) - 1; i >= 0; i-- {
 			if !yield(i) {
 				return
 			}
@@ -61,8 +59,8 @@ func (l *SliceList[V]) ReversePositions() iter.Seq[int] {
 
 func (l *SliceList[V]) ReverseValues() iter.Seq[V] {
 	return func(yield func(V) bool) {
-		for i := len(l.slice) - 1; i >= 0; i-- {
-			if !yield(l.slice[i]) {
+		for i := len(*l) - 1; i >= 0; i-- {
+			if !yield((*l)[i]) {
 				return
 			}
 		}
@@ -70,69 +68,63 @@ func (l *SliceList[V]) ReverseValues() iter.Seq[V] {
 }
 
 func (l *SliceList[V]) First() (int, V, bool) {
-	if len(l.slice) == 0 {
+	if len(*l) == 0 {
 		var zero V
 		return 0, zero, false
 	}
-	return 0, l.slice[0], true
+	return 0, (*l)[0], true
 }
 
 func (l *SliceList[V]) Last() (int, V, bool) {
-	if len(l.slice) == 0 {
+	if len(*l) == 0 {
 		var zero V
 		return 0, zero, false
 	}
-	lastIdx := len(l.slice) - 1
-	return lastIdx, l.slice[lastIdx], true
+	lastIdx := len(*l) - 1
+	return lastIdx, (*l)[lastIdx], true
 }
 
 func (l *SliceList[V]) Set(p int, v V) {
-	if p < 0 || p >= len(l.slice) {
+	if p < 0 || p >= len(*l) {
 		panic("index out of bounds")
 	}
-	l.slice[p] = v
+	(*l)[p] = v
 }
 
 func (l *SliceList[V]) InsertBefore(p int, v V) int {
-	if p < 0 || p > len(l.slice) {
+	if p < 0 || p > len(*l) {
 		panic("index out of bounds")
 	}
-	l.slice = append(l.slice[:p], append([]V{v}, l.slice[p:]...)...)
+	*l = append((*l)[:p], append([]V{v}, (*l)[p:]...)...)
 	return p
 }
 
 func (l *SliceList[V]) InsertAfter(p int, v V) int {
-	if p < 0 || p >= len(l.slice) {
+	if p < 0 || p >= len(*l) {
 		panic("index out of bounds")
 	}
-	l.slice = append(l.slice[:p+1], append([]V{v}, l.slice[p+1:]...)...)
+	*l = append((*l)[:p+1], append([]V{v}, (*l)[p+1:]...)...)
 	return p + 1
 }
 
 func (l *SliceList[V]) Append(v V) int {
-	l.slice = append(l.slice, v)
-	return len(l.slice) - 1
+	*l = append(*l, v)
+	return len(*l) - 1
 }
 
 func (l *SliceList[V]) Remove(p int) {
-	if p < 0 || p >= len(l.slice) {
+	if p < 0 || p >= len(*l) {
 		panic("index out of bounds")
 	}
-	l.slice = append(l.slice[:p], l.slice[p+1:]...)
+	*l = append((*l)[:p], (*l)[p+1:]...)
 }
 
 func (l *SliceList[V]) Reserve(n int) {
-	if l.slice == nil {
-		l.slice = make([]V, 0, n)
+	if *l == nil {
+		*l = make([]V, 0, n)
 	}
 }
 
 func (l *SliceList[V]) Add(v V) {
 	l.Append(v)
-}
-
-func NewSliceList[V any](entries ...V) *SliceList[V] {
-	return &SliceList[V]{
-		slice: entries,
-	}
 }
