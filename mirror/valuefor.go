1b82c68ee793543a9226e7665d48f1b66a9d0268
Andy Kreling <krelinga@gmail.com>
2026-04-24 04:50:43 +0000

    document mirror.ValueFor.

--- mirror/valuefor.go
+++ mirror/valuefor.go
@@ -2,6 +2,13 @@ package mirror
 
 import "reflect"
 
+// ValueFor returns a reflect.Value for the given value v.
+//
+// It is similar to reflect.ValueOf, with some subtle differences:
+// - Interface types are not stripped away, so if v is an interface value then ValueFor will return a reflect.Value of the interface type, not the concrete type.
+// - nil values of interface types will return a valid reflect.Value, and reflect.Value.IsNil() will return true. In contrast, reflect.ValueOf(nil) returns an invalid reflect.Value, and calling IsNil() on it will panic.
+//
+// This function is useful in generic settings where we can capture information about the type of v.
 func ValueFor[T any](v T) reflect.Value {
 	typ := reflect.TypeFor[T]()
 	val := reflect.ValueOf(v)
