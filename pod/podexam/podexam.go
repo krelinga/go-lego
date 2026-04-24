57f520413a4d0dc028390aebfc965ae5e3eecf05
Andy Kreling <krelinga@gmail.com>
2026-04-24 05:33:26 +0000

    podexam.VecEqualFunc.

--- pod/podexam/podexam.go
+++ pod/podexam/podexam.go
@@ -20,3 +20,16 @@ func MapEqualFunc[K comparable, V any](x, y pod.MapView[K, V], f any) (result ex
 	result.Error = exam.AsError(pod.MapEqualFunc(x, y, valueEqual))
 	return
 }
+
+func VecEqualFunc[T any](x, y pod.VecView[T], f any) (result exam.Result) {
+	result = exam.Result{
+		Args: []any{x, y},
+	}
+	valueEqual, err := mirror.WrapFunc2In1Out[T, T, bool](f)
+	if err != nil {
+		result.Error = fmt.Errorf("failed to wrap value equality function: %w", err)
+		return
+	}
+	result.Error = exam.AsError(pod.VecEqualFunc(x, y, valueEqual))
+	return
+}
