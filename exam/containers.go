cc02f13a7ffa2cf931b28660d57481b7f23a1274
Andy Kreling <krelinga@gmail.com>
2026-05-03 23:23:21 +0000

    SliceEqual() and SliceEqualFunc() in exam.

--- exam/containers.go
+++ exam/containers.go
@@ -23,3 +23,24 @@ func MapEqualFunc[K comparable, V any](a, b map[K]V, eq any) *Failure {
 	}
 	return nil
 }
+
+func SliceEqual[V comparable](a, b []V) *Failure {
+	return SliceEqualFunc(a, b, func(x, y V) bool { return x == y })
+}
+
+func SliceEqualFunc[V any](a, b []V, eq any) *Failure {
+	failure := NewFailure2("a", a, "b", b)
+	eqFunc, err := mirror.WrapFunc2In1Out[V, V, bool](eq)
+	if err != nil {
+		return failure.Wrap(err)
+	}
+	if len(a) != len(b) {
+		return failure
+	}
+	for i := range a {
+		if !eqFunc(a[i], b[i]) {
+			return failure
+		}
+	}
+	return nil
+}
--- exam/containers_test.go
+++ exam/containers_test.go
@@ -141,3 +141,137 @@ func TestMapEqualFunc(t *testing.T) {
 		exam.Try(t, exam.Nil(failure))
 	})
 }
+
+func TestSliceEqual(t *testing.T) {
+	cases := []struct {
+		Name          string
+		Loc           exam.Loc
+		Slice1, Slice2 []int
+		WantEqual     bool
+	}{
+		{
+			Name:      "Equal",
+			Loc:       exam.Here(),
+			Slice1:    []int{1, 2, 3},
+			Slice2:    []int{1, 2, 3},
+			WantEqual: true,
+		},
+		{
+			Name:      "DifferentValues",
+			Loc:       exam.Here(),
+			Slice1:    []int{1, 2, 3},
+			Slice2:    []int{1, 2, 99},
+			WantEqual: false,
+		},
+		{
+			Name:      "DifferentLengths",
+			Loc:       exam.Here(),
+			Slice1:    []int{1, 2, 3},
+			Slice2:    []int{1, 2},
+			WantEqual: false,
+		},
+		{
+			Name:      "NilVsEmpty",
+			Loc:       exam.Here(),
+			Slice1:    []int(nil),
+			Slice2:    []int{},
+			WantEqual: true,
+		},
+		{
+			Name:      "BothNil",
+			Loc:       exam.Here(),
+			Slice1:    nil,
+			Slice2:    nil,
+			WantEqual: true,
+		},
+		{
+			Name:      "DifferentOrder",
+			Loc:       exam.Here(),
+			Slice1:    []int{1, 2, 3},
+			Slice2:    []int{3, 2, 1},
+			WantEqual: false,
+		},
+	}
+	for _, c := range cases {
+		exam.Run(t, c.Name, c.Loc, func(t *testing.T) {
+			failure := exam.SliceEqual(c.Slice1, c.Slice2)
+			if c.WantEqual {
+				exam.Try(t, exam.Nil(failure))
+			} else {
+				exam.Must(t, exam.NotNil(failure))
+			}
+		})
+	}
+}
+
+func TestSliceEqualFunc(t *testing.T) {
+	cases := []struct {
+		Name           string
+		Loc            exam.Loc
+		Slice1, Slice2 []string
+		EqualFunc      func(string, string) bool
+		WantEqual      bool
+	}{
+		{
+			Name:      "Equal",
+			Loc:       exam.Here(),
+			Slice1:    []string{"a", "b"},
+			Slice2:    []string{"a", "b"},
+			EqualFunc: func(s1, s2 string) bool { return s1 == s2 },
+			WantEqual: true,
+		},
+		{
+			Name:      "CaseInsensitiveEqual",
+			Loc:       exam.Here(),
+			Slice1:    []string{"Hello", "World"},
+			Slice2:    []string{"hello", "WORLD"},
+			EqualFunc: func(s1, s2 string) bool { return strings.EqualFold(s1, s2) },
+			WantEqual: true,
+		},
+		{
+			Name:      "DifferentValues",
+			Loc:       exam.Here(),
+			Slice1:    []string{"a", "b"},
+			Slice2:    []string{"a", "z"},
+			EqualFunc: func(s1, s2 string) bool { return s1 == s2 },
+			WantEqual: false,
+		},
+		{
+			Name:      "DifferentLengths",
+			Loc:       exam.Here(),
+			Slice1:    []string{"a", "b"},
+			Slice2:    []string{"a"},
+			EqualFunc: func(s1, s2 string) bool { return s1 == s2 },
+			WantEqual: false,
+		},
+		{
+			Name:      "NilVsEmpty",
+			Loc:       exam.Here(),
+			Slice1:    []string(nil),
+			Slice2:    []string{},
+			EqualFunc: func(s1, s2 string) bool { return s1 == s2 },
+			WantEqual: true,
+		},
+	}
+	for _, c := range cases {
+		exam.Run(t, c.Name, c.Loc, func(t *testing.T) {
+			failure := exam.SliceEqualFunc(c.Slice1, c.Slice2, c.EqualFunc)
+			if c.WantEqual {
+				exam.Try(t, exam.Nil(failure))
+			} else {
+				exam.Must(t, exam.NotNil(failure))
+			}
+		})
+	}
+
+	t.Run("EqualAsAny", func(t *testing.T) {
+		slice1 := []string{"a", "b"}
+		slice2 := []string{"a", "b"}
+		failure := exam.SliceEqualFunc(slice1, slice2, func(s1, s2 any) bool {
+			v1 := exam.MustCast[string](t, s1)
+			v2 := exam.MustCast[string](t, s2)
+			return v1 == v2
+		})
+		exam.Try(t, exam.Nil(failure))
+	})
+}
