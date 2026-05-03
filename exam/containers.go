5af455beec555c58c0247f396a4df468479303c9
Andy Kreling <krelinga@gmail.com>
2026-05-03 23:38:34 +0000

    documentation comments on exam container methods.

--- exam/containers.go
+++ exam/containers.go
@@ -2,10 +2,14 @@ package exam
 
 import "github.com/krelinga/go-lego/mirror"
 
+// MapEqual checks if two maps are equal by comparing their keys and values using the == operator.
 func MapEqual[K, V comparable](a, b map[K]V) *Failure {
 	return MapEqualFunc(a, b, func(x, y V) bool { return x == y })
 }
 
+// MapEqualFunc checks if two maps are equal by comparing their keys and values using a custom equality function.
+// The eq parameter should be a function that takes two values of type U (where V is assignable to U) and returns
+// a bool indicating whether they are considered equal.
 func MapEqualFunc[K comparable, V any](a, b map[K]V, eq any) *Failure {
 	failure := NewFailure2("a", a, "b", b)
 	eqFunc, err := mirror.WrapFunc2In1Out[V, V, bool](eq)
@@ -24,10 +28,14 @@ func MapEqualFunc[K comparable, V any](a, b map[K]V, eq any) *Failure {
 	return nil
 }
 
+// SliceEqual checks if two slices are equal by comparing their elements using the == operator.
 func SliceEqual[V comparable](a, b []V) *Failure {
 	return SliceEqualFunc(a, b, func(x, y V) bool { return x == y })
 }
 
+// SliceEqualFunc checks if two slices are equal by comparing their elements using a custom equality function.
+// The eq parameter should be a function that takes two values of type U (where V is assignable to U) and returns
+// a bool indicating whether they are considered equal.
 func SliceEqualFunc[V any](a, b []V, eq any) *Failure {
 	failure := NewFailure2("a", a, "b", b)
 	eqFunc, err := mirror.WrapFunc2In1Out[V, V, bool](eq)
