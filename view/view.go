83afcbab754f4407bf3aeb7cc69e374c91badc6b
Andy Kreling <krelinga@gmail.com>
2026-05-07 15:49:23 +0000

    delete view.Direct.

--- view/view.go
+++ view/view.go
@@ -3,15 +3,3 @@ package view
 type Viewer[T any] interface {
 	View() T
 }
-
-func Direct[T any](v T) Viewer[T] {
-	return direct[T]{v: v}
-}
-
-type direct[T any] struct {
-	v T
-}
-
-func (d direct[T]) View() T {
-	return d.v
-}
