1b85f95ee8c9331e9bd6a86c9df2f04e51a33193
Andy Kreling <krelinga@gmail.com>
2026-05-08 01:47:53 +0000

    merge podexam into exam package.

--- exam/pod.go
+++ exam/pod.go
@@ -1,15 +1,14 @@
-package podexam
+package exam
 
 import (
 	"fmt"
 
-	"github.com/krelinga/go-libs/exam"
 	"github.com/krelinga/go-libs/mirror"
 	"github.com/krelinga/go-libs/pod"
 )
 
-func DictEqualFunc[K comparable, V any](x, y pod.FixedDict[K, V], f any) *exam.Failure {
-	failure := exam.NewFailure2("x", x, "y", y)
+func DictEqualFunc[K comparable, V any](x, y pod.FixedDict[K, V], f any) *Failure {
+	failure := NewFailure2("x", x, "y", y)
 	valueEqual, err := mirror.WrapFunc2In1Out[V, V, bool](f)
 	if err != nil {
 		return failure.Wrap(fmt.Errorf("failed to wrap value equality function: %w", err))
@@ -19,8 +18,8 @@ func DictEqualFunc[K comparable, V any](x, y pod.FixedDict[K, V], f any) *exam.F
 	return failure
 }
 
-func VecEqualFunc[T any](x, y pod.FixedVec[T], f any) *exam.Failure {
-	failure := exam.NewFailure2("x", x, "y", y)
+func VecEqualFunc[T any](x, y pod.FixedVec[T], f any) *Failure {
+	failure := NewFailure2("x", x, "y", y)
 	valueEqual, err := mirror.WrapFunc2In1Out[T, T, bool](f)
 	if err != nil {
 		return failure.Wrap(fmt.Errorf("failed to wrap value equality function: %w", err))
