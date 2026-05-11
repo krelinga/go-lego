93b062f27352668594e0c5c8b97d81d6952a4a53
Andy Kreling <krelinga@gmail.com>
2026-05-11 05:08:57 +0000

    simplify As() in mirror and match.

--- match/as.go
+++ match/as.go
@@ -4,16 +4,12 @@ import (
 	"reflect"
 
 	"github.com/krelinga/go-libs/mirror"
-	"github.com/krelinga/go-libs/zero"
 )
 
 func As[T any](h *Helper, val reflect.Value) (T, error) {
-	if err := h.CheckValid(); err != nil {
-		return zero.For[T](), err
-	}
-	tVal, ok := mirror.As[T](val)
-	if !ok {
-		return tVal, h.Fatalf("value is not of expected type %T", tVal)
+	tVal, err := mirror.As[T](val)
+	if err != nil {
+		return tVal, h.Fatalf("cannot convert value to type %s: %w", reflect.TypeFor[T](), err)
 	}
 	return tVal, nil
 }
--- mirror/as.go
+++ mirror/as.go
@@ -1,14 +1,22 @@
 package mirror
 
 import (
+	"fmt"
 	"reflect"
 
 	"github.com/krelinga/go-libs/zero"
 )
 
-func As[T any](val reflect.Value) (T, bool) {
-	if !val.IsValid() || !val.Type().AssignableTo(reflect.TypeFor[T]()) {
-		return zero.For[T](), false
+func As[T any](val reflect.Value) (T, error) {
+	if !val.IsValid() {
+		return zero.For[T](), fmt.Errorf("value is invalid")
 	}
-	return val.Interface().(T), true
+	tType := reflect.TypeFor[T]()
+	if !val.Type().AssignableTo(tType) {
+		return zero.For[T](), fmt.Errorf("value of type %s cannot be assigned to type %s", val.Type(), tType)
+	}
+	if !val.CanInterface() {
+		return zero.For[T](), fmt.Errorf("value cannot be interfaced")
+	}
+	return val.Interface().(T), nil
 }
