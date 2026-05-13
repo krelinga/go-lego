7f16f5dfb6825180d5faf0a0373974151f2797c7
Andy Kreling <krelinga@gmail.com>
2026-05-13 19:39:02 +0000

    basic tests for Nil.

---
+++ match/nil_test.go
@@ -0,0 +1,12 @@
+package match_test
+
+import (
+	"testing"
+
+	m "github.com/krelinga/go-libs/match"
+)
+
+func TestNil(t *testing.T) {
+	tryMatch(t, nil, m.Nil{})
+	tryMatch(t, &struct{}{}, m.Not(m.Nil{}))
+}
