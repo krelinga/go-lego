3f44f38c0a3ed9e9b71add1257be03ffac7461dd
Andy Kreling <krelinga@gmail.com>
2026-05-13 19:21:14 +0000

    Not matcher.

---
+++ match/not.go
@@ -0,0 +1,30 @@
+package match
+
+import "fmt"
+
+type not struct {
+	Matcher Matcher
+}
+
+func (n *not) Validate() error {
+	if n.Matcher == nil {
+		return fmt.Errorf("not matcher must have a non-nil Matcher field")
+	}
+	return n.Matcher.Validate()
+}
+
+func (n *not) Match(val any) (*Result, error) {
+	h := &Helper{
+		Meta: MetaHere(),
+		Val:  val,
+	}
+	childAccepted, err := h.Child("", val, n.Matcher)
+	if err != nil {
+		return nil, err
+	}
+	if childAccepted {
+		return h.Reject("child matcher accepted"), nil
+	} else {
+		return h.Accept("child matcher rejected"), nil
+	}
+}
