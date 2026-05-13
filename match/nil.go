01b887564901b607ebb8f619fb07ff519bef31cc
Andy Kreling <krelinga@gmail.com>
2026-05-13 19:17:30 +0000

    Nil matcher.

---
+++ match/nil.go
@@ -0,0 +1,31 @@
+package match
+
+import "reflect"
+
+type Nil struct{}
+
+func (n Nil) Validate() error {
+	return nil
+}
+
+func (n Nil) Match(val any) (*Result, error) {
+	h := &Helper{
+		Meta: MetaHere(),
+		Val:  val,
+	}
+	const acceptStr = "value is nil"
+
+	if val == nil {
+		return h.Accept(acceptStr), nil
+	}
+	rVal := reflect.ValueOf(val)
+	switch rVal.Kind() {
+	case reflect.Interface, reflect.Pointer, reflect.Slice, reflect.Map, reflect.Chan, reflect.Func:
+		if rVal.IsNil() {
+			return h.Accept(acceptStr), nil
+		} else {
+			return h.Reject("value is not nil"), nil
+		}
+	}
+	return nil, h.Fatalf("Nil matcher only accepts nil-able types, but got type %s", rVal.Type())
+}
